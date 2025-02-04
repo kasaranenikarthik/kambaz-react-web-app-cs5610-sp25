4e7ae3be3f218a83f161008c34686f4e5875fb1d
karthikkgvs <kasaraneni.g@northeastern.edu>
2025-02-04 10:01:26 -0500

    A2 final

--- src/Kambaz/Account/Navigation.tsx
+++ src/Kambaz/Account/Navigation.tsx
@@ -1,9 +1,15 @@
 import { Link } from "react-router-dom";
 export default function AccountNavigation() {
   return (
-    <div id="wd-account-navigation">
-      <Link to={`/Kambaz/Account/Signin`}  > Signin  </Link> <br/>
-      <Link to={`/Kambaz/Account/Signup`}  > Signup  </Link> <br/>
-      <Link to={"/Kambaz/Account/Profile"} > Profile </Link> <br/>
+    <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
+      <Link to={`/Kambaz/Account/Signin`} className="list-group-item list-group-item-action active border border-0" > 
+        Signin  
+      </Link> <br/>
+      <Link to={`/Kambaz/Account/Signup`}  className="list-group-item list-group-item-action text-danger border border-0">
+        Signup  
+      </Link> <br/>
+      <Link to={"/Kambaz/Account/Profile"} className="list-group-item list-group-item-action text-danger border border-0"> 
+        Profile 
+      </Link> <br/>
     </div>
 );}
