0d0403b5f7a455e7b448a8f940c9990fca773101
karthikkgvs <kasaraneni.g@northeastern.edu>
2025-03-12 23:46:05 -0400

    a4 Redux

--- src/Kambaz/Courses/ProtectedCourseRoute.tsx
+++ src/Kambaz/Courses/ProtectedCourseRoute.tsx
@@ -5,7 +5,7 @@ export default function ProtectedCourseRoute({ children, cid }: { children: any,
   const { currentUser } = useSelector((state: any) => state.accountReducer);
   const { enrollments }  = useSelector((state: any) => state.enrollReducer);
 
-  if (enrollments.some((enrollment: any) => enrollment.user === currentUser._id && enrollment.course === cid)) {
+  if (enrollments.filter((enrollment: any) => enrollment.user === currentUser._id && enrollment.course === cid).length > 0) {
     return children;
   } else {
     return <Navigate to="/Kambaz/Dashboard" />;
