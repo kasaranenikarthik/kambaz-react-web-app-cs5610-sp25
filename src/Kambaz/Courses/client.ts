370e590f79658290022f229a5ace912fbd4b8c86
karthikkgvs <kasaraneni.g@northeastern.edu>
2025-04-14 19:11:20 -0400

    A6

--- src/Kambaz/Courses/client.ts
+++ src/Kambaz/Courses/client.ts
@@ -1,9 +1,13 @@
 import axios from "axios";
+
 const REMOTE_SERVER = import.meta.env.VITE_REMOTE_SERVER;
 const COURSES_API = `${REMOTE_SERVER}/api/courses`;
+const axiosWithCredentials = axios.create({
+  withCredentials: true,   // This will include cookies in requests
+});
 
 export const fetchAllCourses = async () => {
-  const { data } = await axios.get(COURSES_API);
+  const { data } = await axiosWithCredentials.get(COURSES_API);
   return data;
 };
 
